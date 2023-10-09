823dfa24f5f96f63886868259fc766b7793aaeef
Alina Potapchuk <alinapotapchuk7@gmail.com>
2023-10-09 22:59:43 +0300

    Initial data

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import { useReducer } from 'react';
+
+function App() {
+  return (
+    <div className="page">
+      <h1 className='tiles__title'>Tiles Game</h1>
+    </div> 
+  );
+}
+
+export default App;
